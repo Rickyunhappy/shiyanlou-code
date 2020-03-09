36b87b56dbb77a5d2189aea3d70b636ca960a73f
Rickyunhappy <renkai0531@163.com>
2020-03-09 19:47:07 +0800

    4th commit

--- calculator.py
+++ calculator.py
@@ -1,11 +1,12 @@
 ##定义变量
-income = int(input("请输入你的薪资： "))
 salary = 0
 shouldPay = 0
 tax = 0
+import sys
 
 def calculator(num):
-    shouldPay = num - 5000
+##减去五险一金
+    shouldPay = num *(1-0.165) - 5000
     if shouldPay <= 0:
         tax = 0
     elif 0 < shouldPay <= 3000:
@@ -23,7 +24,23 @@ def calculator(num):
     else:
         tax = shouldPay * 0.45 -15160 
 
-    salary = num - tax
+    salary = num * (1-0.165) - tax
     return '{:.2f}'.format(salary)
 
-print('你的税后收入是：{}'.format(calculator(income)))
+##对命令行传入的每一个用户，依次调用计算器
+def main():
+    ##获取id:income的每个元素
+    for item in sys.argv[1:]:
+        ##把id:income分割成两个元素
+        id, income = item.split(':')
+        ##定义可能的报错：无法将薪资转化为整数形式
+        try:
+            income = int(income)
+        ##如果无法转化为整数，打印提示文字
+        except ValueError:
+            print('请在薪资位置输入数字')
+            continue #跳过当前异常值
+        print("{}:{}".format(id, calculator(income)))
+
+if __name__ == '__main__':
+    main()
